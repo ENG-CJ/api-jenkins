54fbb5bdc20d0a508cd672e89397644477a01084
Abdulrahman-Haji <77126546+ENG-CJ@users.noreply.github.com>
2024-12-03 09:25:21 +0300

    user credentials added

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get("/",(req,res)=>{
     return res.send({message : "server is running", status: true, jenkins: true, pipline: "starting"})
 })
 
+app.get("/user", (req, res) => {
+  return res.send({
+   user: "Abdulrahman",
+   IPO: "X012LSJ"
+  });
+});
+
 app.listen(3400,'0.0.0.0',()=>{
     console.log("started")
 })
